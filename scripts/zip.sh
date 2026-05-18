4626e4d7f1a8e512a060c39c1b17bef30746d89f
sobhan nourani <102912400+sbn-program@users.noreply.github.com>
2026-05-18 07:42:09 -0700

    Update zip.sh

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ "$ZIP_MODE" != "zip" ]; then
-  echo "ℹ️ ZIP mode disabled"
-  exit 0
-fi
-
-if [ -n "$ZIP_PASSWORD" ]; then
-  zip -P "$ZIP_PASSWORD" -r output.zip downloads
-else
+if [ "$ZIP_MODE" = "zip" ]; then
+  if [ -n "$ZIP_PASSWORD" ]; then
+    zip -P "$ZIP_PASSWORD" -r output.zip downloads
+  else
+    zip -r output.zip downloads
+  fi
+elif [ "$ZIP_MODE" = "split" ]; then
   zip -r output.zip downloads
+else
+  exit 0
 fi
 
 echo "✅ ZIP created"
