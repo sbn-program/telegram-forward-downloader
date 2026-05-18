1025cff41cce9f6c4b7652917a354809bb2a23ef
sobhan nourani <102912400+sbn-program@users.noreply.github.com>
2026-05-18 09:44:56 -0700

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,16 +3,13 @@ set -e
 
 mkdir -p downloads
 
-INDEX=1
-
 echo "$URLS" | while read -r URL; do
   [ -z "$URL" ] && continue
 
-  DIR="downloads/file_$INDEX"
+  ID=$(openssl rand -hex 5)
+  DIR="downloads/$ID"
   mkdir -p "$DIR"
 
   echo "⬇️ Downloading to $DIR"
   aria2c -x16 -s16 -k1M "$URL" -d "$DIR"
-
-  INDEX=$((INDEX + 1))
 done
