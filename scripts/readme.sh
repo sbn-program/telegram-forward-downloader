26227557209ba0637bd6ccb0a58a0e3794ee42cc
sobhan nourani <102912400+sbn-program@users.noreply.github.com>
2026-05-18 08:56:52 -0700

    Update readme.sh

--- scripts/readme.sh
+++ scripts/readme.sh
@@ -7,18 +7,17 @@ REPO_URL="https://github.com/${GITHUB_REPOSITORY}"
 cat > README.md <<EOF
 # 📦 Download Center
 
-✅ Generated automatically  
+✅ Auto Generated  
 🕒 $(date)
 
-## 🔗 Download Links
-
+## 🔗 Download Parts
 EOF
 
-for f in output/*; do
+for f in output/output.part.*; do
   FILE=$(basename "$f")
   echo "- [$FILE]($REPO_URL/raw/$BRANCH/output/$FILE)" >> README.md
 done
 
 git add README.md
 git commit -m "📝 Update README"
-git push origin "$BRANCH" --force
+git push origin "$BRANCH"
